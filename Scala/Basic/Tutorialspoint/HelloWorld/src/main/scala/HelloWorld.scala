82d597ce92216a75c149e4cd2230fb90c751c56b
Roberto Foglia <fogliaroberto@hotmail.it>
2022-07-12 17:38:32 +0200

    Init project and helloworld

---
+++ Scala/Basic/Tutorialspoint/HelloWorld/src/main/scala/HelloWorld.scala
@@ -0,0 +1,19 @@
+object HelloWorld extends App {
+  // https://www.tutorialspoint.com/scala/scala_basic_syntax.htm
+
+  /* This is my first java program.
+  * This will print 'Hello World' as the output
+  */
+  println("Hello, world!\n") // prints Hello World
+
+  // multiline string
+  println(
+    """
+    |Multiline string
+    |
+    |the present string
+      |spans three
+      |lines.""".stripMargin
+  )
+
+}
