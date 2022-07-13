5baea91c04d5fcff0be88ab07cc6be855b96e134
Roberto Foglia <fogliaroberto@hotmail.it>
2022-07-13 11:16:34 +0200

    Init project, helloworld and data types

--- Scala/Basic/Tutorialspoint/HelloWorld/src/main/scala/HelloWorld.scala
+++ Scala/Basic/Tutorialspoint/HelloWorld/src/main/scala/HelloWorld.scala
@@ -16,4 +16,28 @@ object HelloWorld extends App {
       |lines.""".stripMargin
   )
 
+
+  //#################################################
+  // val or val VariableName : DataType = [Initial Value]
+  //#################################################
+
+  // var --> mutable variable
+  var myVarDef : String = "Foo"
+
+  // val --> immutable variable
+  val myValDef : String = "Foo"
+
+  // Variable Type Inference
+  val typeInference = ""
+
+  //##################################################
+
+
+  var myVar :Int = 10;
+  val myVal :String = "Hello Scala with datatype declaration.";
+  var myVar1 = 20;
+  val myVal1 = "Hello Scala new without datatype declaration.";
+
+  println(myVar); println(myVal); println(myVar1);
+  println(myVal1);
 }
