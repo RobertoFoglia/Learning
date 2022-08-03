6eb0891c19e4959ca97d50f351f4468bca669329
Roberto Foglia <fogliaroberto@hotmail.it>
2022-08-03 12:18:15 +0200

    Main examples

---
+++ Scala/Basic/Tutorialspoint/HelloWorld/src/main/scala/MainExamples.scala
@@ -0,0 +1,10 @@
+object MainExamples {
+    /** Usage: scala rounding.TryCatch filename1 filename2 ... */
+    def main(args: Array[String]): Unit = {
+      args foreach println
+    }
+}
+
+object MainExamples2 extends App {
+  args foreach println
+}
