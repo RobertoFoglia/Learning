20f812cc1666b0b429c0e604046d3109479c738c
Roberto Foglia <roberto.foglia@vtsolutions.it>
2024-03-12 12:30:27 +0100

    Lambdas > API functional interfaces > Function and BiFunction

--- Java/Language/Java 21 17 11 and 8/Lambdas/src/main/java/rfoglia/java/language/APIFunctionalInterfaces.java
+++ Java/Language/Java 21 17 11 and 8/Lambdas/src/main/java/rfoglia/java/language/APIFunctionalInterfaces.java
@@ -95,20 +95,25 @@ public class APIFunctionalInterfaces {
     }
     // ##########################################
 
+
+    // @@@ Function and BiFunction   ########################
     public void function() {
-        // Function<T, R> is a functional interface i.e. one abstract method:
-        //      R apply(T t)
+        //  Function<T, R> is a functional interface i.e. one abstract method:
+        //       R apply(T t)
         Function<String, Integer> fn2 = s -> s.length();
         System.out.println("Function: " + fn2.apply("Moscow"));// 6
 
-        // BiFunction<T, U, R> is a functional interface i.e. one abstract method:
-        //      R apply(T t, U u)
+        //  BiFunction<T, U, R> is a functional interface i.e. one abstract method:
+        //       R apply(T t, U u)
         BiFunction<String, String, Integer> biFn   = (s1, s2) -> s1.length() + s2.length();
         System.out.println("BiFunction: " + biFn.apply("William", "Shakespeare"));// 18
 
         BiFunction<String, String, String> biFn2   = (s1, s2) -> s1.concat(s2);
         System.out.println("BiFunction: " + biFn2.apply("William ", "Shakespeare"));// William Shakespeare
     }
+    // ##########################################
+
+
     public void unaryBinaryOperator() {
         // UnaryOperator<T> extends Function<T, T> is a functional interface i.e. one abstract method:
         //      T apply(T t)
